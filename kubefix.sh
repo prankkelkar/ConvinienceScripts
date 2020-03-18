8392f055b2b8706f4b25a3bd38448cb96120b783
Prasanna Kelkar <prankkelkar@gmail.com>
2020-03-18 18:08:02 +0530

    Update kubefix.sh

--- kubefix.sh
+++ kubefix.sh
@@ -7,8 +7,11 @@ KUBERNETES_VERSION="$(curl -s https://storage.googleapis.com/kubernetes-release/
 # Print the usage message
 function printHelp() {
 	echo
+	echo "This script is intented to resolve kube-proxy issue in kubernetes v1.17.x for s390x architecture"
+	echo "Issue link: https://github.com/kubernetes/kubernetes/issues/87197"
+	echo "Run this script before initializing kubernetes cluster.(Before running 'kubeadm init' command) "
 	echo "Usage: "
-	echo "Please make sure u have make, curl and git "
+	echo "Please make sure you have make, curl and git Installed "
 	echo "  kubefix.sh [-v kubernetes version]  "
 	echo "  For eg: bash kubefix.sh -v v1.17.3  "
 	echo
@@ -30,11 +33,12 @@ while getopts "h?dy?v:" opt; do
 	esac
 done
 
+#Fist we build debian-base image on s390x since there are issues in cross-compiled image.
 git clone https://github.com/kubernetes/kubernetes.git
 cd kubernetes/build/debian-iptables
 make build ARCH=s390x
 
-
+#Copy the kube proxy binary from the image
 cd $CURDIR
 docker run --rm -v $CURDIR:/tmp k8s.gcr.io/kube-proxy:$KUBERNETES_VERSION cp /usr/local/bin/kube-proxy /tmp
 
@@ -46,3 +50,4 @@ EOF
 
 docker build . -t k8s.gcr.io/kube-proxy:$KUBERNETES_VERSION
 
+echo "Kube-proxy image is built successfully. You can proceed with 'kubeadm init' "
